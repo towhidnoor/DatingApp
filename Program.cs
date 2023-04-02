02b972ccff142624b042fa357a9a3be2a2e64469
Towhid <tnailyrami@gmail.com>
2023-04-02 15:51:51 +0600

    End of Section #

--- Program.cs
+++ Program.cs
@@ -10,12 +10,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 //* Anything above app = builder.buid() is for adding services and anything below is for Http request *//
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app .UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
